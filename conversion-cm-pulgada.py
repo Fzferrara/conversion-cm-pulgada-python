dbe19b6cfc59f062f79111630d563a577ac54712
Fzferrara <fzferrara@gmail.com>
2024-12-17 18:14:33 +0100

    Realizamos un programa para calcular la conversion de cm a pulgadas

---
+++ conversion-cm-pulgada.py
@@ -0,0 +1,12 @@
+
+
+
+# Paso 1: Solicitar al usuario la medida en cm a convertir, definiendo cm.
+
+cm = float(input("Ingrese la medidad en cm a convertir: "))
+
+# Paso 2: Realizar la conversion.
+resultado = cm * 0.393701
+
+# Paso 4: Mostrar el resultado de la conversion al usuario.
+print("El resultado de la conversion de cm a Pulgada es:" , resultado )
